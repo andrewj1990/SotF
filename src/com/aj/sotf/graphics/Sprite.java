a92e6315252e9f2a706a7a76639ecb8f407992c5
Andrew Jackson <asj@uvic.ca>
2015-12-09 20:50:01 -0800

    added water animation (needs fixing)

--- src/com/aj/sotf/graphics/Sprite.java
+++ src/com/aj/sotf/graphics/Sprite.java
@@ -13,6 +13,7 @@ public class Sprite {
 	public static Sprite s1 = new Sprite(0, 0, 16, SpriteSheet.tiles);
 	public static Sprite s2 = new Sprite(1, 0, 16, SpriteSheet.tiles);
 	public static Sprite water = new Sprite(0, 1, 16, SpriteSheet.tiles);
+	public static Sprite water2 = new Sprite(1, 1, 16, SpriteSheet.tiles);
 	public static Sprite dirt = new Sprite(1, 0, 16, SpriteSheet.tiles);
 	
 	public Sprite(int x, int y, int size, SpriteSheet sheet) {
--- src/com/aj/sotf/level/tile/WaterTile.java
+++ src/com/aj/sotf/level/tile/WaterTile.java
@@ -1,11 +1,32 @@
 package com.aj.sotf.level.tile;
 
+import com.aj.sotf.graphics.Screen;
 import com.aj.sotf.graphics.Sprite;
 
 public class WaterTile extends Tile {
 	
+	private int anim;
+	
 	public WaterTile(Sprite sprite) {
 		super(sprite);
+		anim = 0;
+	}
+	
+	public void render(int xx, int yy, float intensity, Screen screen) {
+		if (anim < 10000000) {
+			anim++;
+		} else {
+			anim = 0;
+		}
+//		System.out.println("anim :" + anim);
+		
+		if (anim % 500000 > 250000) {
+			sprite = Sprite.water2;
+		} else {
+			sprite = Sprite.water;
+		}
+		
+		sprite.render(xx, yy, intensity, screen);
 	}
 	
 	public boolean solid() {
